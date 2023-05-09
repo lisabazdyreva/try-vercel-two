08e234b552d8ded3333b65e2cf0c849b3e13fe9f
Lisa <lisabazdyreva@gmail.com>
2023-05-09 18:19:53 +0300

    try change router

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 
+const router = require("./router/index.js");
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
+app("/api", router);
 
 app.listen(5000, () => console.log(`Server started on port ${5000}`));
 module.exports = app;
