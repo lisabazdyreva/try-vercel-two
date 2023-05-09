ab697d0f4ce55f2048cc6ca7bcade2901a074c3d
Lisa <lisabazdyreva@gmail.com>
2023-05-09 18:20:58 +0300

    try change router

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const router = require("./router/index.js");
 
 app.use(express.json());
 
-app("/api", router);
+app("api", router);
 
 app.listen(5000, () => console.log(`Server started on port ${5000}`));
 module.exports = app;
